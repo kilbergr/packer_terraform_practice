954e655e4dfb50477a423186fe739ad6b4e442a9
Rebecca Kilberg <rekilberg@gmail.com>
2017-07-07 11:32:39 -0700

    modifying runapp script and separating the work on the systemd service out

--- run_app.sh
+++ run_app.sh
@@ -8,16 +8,3 @@ source aws_project/bin/activate
 
 # Run Python app
 python manage.py runserver 0.0.0.0:8000
-
-
-# BELOW: This is what the above looks like in a systemd service
-# [Unit]
-# Description=My Django Python Script Service
-# After=network.target
-
-# [Service]
-# Type=simple
-# ExecStart=/home/ubuntu/instance_name/aws_project/bin/python /home/ubuntu/instance_name/manage.py runserver 0.0.0.0:8000
-
-# [Install]
-# WantedBy=multi-user.target
